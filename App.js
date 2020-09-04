8e51bf26a2df0a11c9f15f36cf10c3dc1724de3f
Doni Setiawan <donisetiawan.050@gmail.com>
2020-09-04 12:46:41 +0700

    Components

--- App.js
+++ App.js
@@ -1,22 +1,173 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
+import SearchInput from './components/SearchInput';
+import getImageForWeather from './utils/getImageForWeather';
+import { fetchLocationId, fetchWeather } from './utils/api';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily:
+      Platform.OS === 'ios'
+        ? 'AvenirNext-Regular'
+        : 'Roboto',
+    color: 'white',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
   },
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      error: false,
+      location: '',
+      temperature: 0,
+      weather: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('dubai');
+  }
+
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const {
+          location,
+          weather,
+          temperature,
+        } = await fetchWeather(locationId);
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
+  render() {
+    const {
+      loading,
+      error,
+      location,
+      weather,
+      temperature,
+    } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator
+              animating={loading}
+              color="white"
+              size="large"
+            />
+            {!loading && (
+              <View>
+                {error && (
+                  <Text
+                    style={[
+                      styles.smallText,
+                      styles.textStyle,
+                    ]}
+                  >
+                    Could not load weather, please try a
+                    different city.
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text
+                      style={[
+                        styles.largeText,
+                        styles.textStyle,
+                      ]}
+                    >
+                      {location}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.smallText,
+                        styles.textStyle,
+                      ]}
+                    >
+                      {weather}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.largeText,
+                        styles.textStyle,
+                      ]}
+                    >
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
